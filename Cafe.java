30bd24ed9a7b822e236bac8067a5f107062fd65c
govenERR <lfairman@smith.edu>
2024-11-10 11:51:29 -0500

    Update Cafe.java

--- Cafe.java
+++ Cafe.java
@@ -1,48 +1,83 @@
-/* This is a stub for the Cafe class */
-public class Cafe extends Building {
-    private int nCoffeeOunces;
-    private int nSugarPackets;
-    private int nCreams;
-    private int nCups;
+/* This is a stub for the House class */
+import java.util.ArrayList;
 
-    public Cafe(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups, String name, String address, int nFloors) {
-        super(name, address, nFloors);
-        this.nCoffeeOunces = nCoffeeOunces;
-        this.nSugarPackets = nSugarPackets;
-        this.nCreams = nCreams;
-        this.nCups = nCups;
-        System.out.println("You have built a cafe: ☕");
-    }
+public class House extends Building {
+  
+  private boolean hasDiningRoom;
+  private ArrayList<String> residents;
+  private boolean hasElevator;
 
-    public void sellCoffee(int size, int sugar, int creams) {
-        if (this.nCoffeeOunces < size) {
-            restock(size + 8, 0, 0, 0);
-        }
-        this.nCoffeeOunces += -size;
-        if (this.nSugarPackets < sugar) {
-            restock(0, sugar + 5, 0, 0);
-        }
-        this.nSugarPackets += -sugar;
-        if (this.nCreams < creams) {
-            restock(0, 0, creams + 3, 0);
-        }
-        this.nCreams += -creams;
-        if (this.nCups == 0) {
-            restock(0,0,0,5);
-        }
-        this.nCups += -1;
+  public House(boolean hasDiningRoom, ArrayList<String> residents, boolean hasElevator, String name, String address, int nFloors) {
+    super(name, address, nFloors);
+    this.hasDiningRoom = hasDiningRoom;
+    this.residents = residents;
+    this.hasElevator = hasElevator;
+    System.out.println("You have built a house: 🏠");
+  }
+  public boolean hasDiningRoom() {
+    return this.hasDiningRoom;
+  }
+  public boolean hasElevator() {
+    return this.hasElevator;
+  }
+  public int numResidents() {
+    return this.residents.size();
+  }
+  public void moveIn(String name) {
+    this.residents.add(name);
+  }
+  public void moveIn(String name1, String name2) {
+    this.residents.add(name1);
+    this.residents.add(name2);
+  }
+  public void moveOut(String name) {
+    this.residents.remove(name);
+  }
+  public void moveOut(String name1, String name2) {
+    this.residents.remove(name1);
+    this.residents.remove(name2);
+  }
+  public boolean isResident(String name) {
+    return this.residents.contains(name);
+  }
+  public void showOptions() {
+    super.showOptions();
+    System.out.println( " + hasDiningRoom() \n + numResidents() \n + moveIn(name) \n + moveOut(name) \n + isResident(name)");
+  }
+  public void goToFloor(int floorNum) {
+    if (floorNum - 1 != activeFloor && floorNum + 1 != activeFloor) {
+      if (this.hasElevator == false) {
+        System.out.println("You cannot go directly to this floor because your house does not have an elevator! Please take the stairs one at a time.");
+        return;
+      }
+      else {
+        super.goToFloor(floorNum);
+      }
     }
+    else {
+      super.goToFloor(floorNum);
+    }
+  }
 
-    private void restock(int ncoffee, int nsugar, int ncreams, int ncups) {
-        this.nCoffeeOunces += ncoffee;
-        this.nSugarPackets += nsugar;
-        this.nCreams += ncreams;
-        this.nCups += ncups;
-    }
-    
-    public static void main(String[] args) {
-        Cafe compass = new Cafe(12,6,6, 4, "Compass Cafe", "7 Neilson Street", 1);
-        System.out.println(compass.getAddress());
-    }
-    
+
+  public static void main(String[] args) {
+    ArrayList<String> gspotResidents = new ArrayList<String>();
+    gspotResidents.add("Molly");
+    gspotResidents.add("Alex");
+    House gardinerHouse = new House(false,gspotResidents, false, "Gardiner", "1 Paradise Rd", 4);
+    System.out.println(gardinerHouse.hasDiningRoom());
+    System.out.println(gardinerHouse.numResidents());
+    gardinerHouse.moveIn("Lucia");
+    gardinerHouse.moveOut("Alex");
+    System.out.println(gardinerHouse.isResident("Alex"));
+    System.out.println(gardinerHouse.isResident("Lucia"));
+    gardinerHouse.showOptions();
+    gardinerHouse.enter();
+    gardinerHouse.goToFloor(2);
+    gardinerHouse.goUp();
+    gardinerHouse.goDown();
+    gardinerHouse.moveIn("Lily", "Sabina");
+    System.out.println(gspotResidents);
+  }
+
 }
